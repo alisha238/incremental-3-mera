9e7069e76e82f6d2de7578b92946a5fa7bdc70f6
Iamneo <techadmin@iamneo.ai>
2023-11-22 05:39:34 +0000

    Backup Commit

--- angularapp/src/app/delete-player/delete-player.component.ts
+++ angularapp/src/app/delete-player/delete-player.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AdminService } from '../services/admin.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Player } from '../models/player';
 
 @Component({
   selector: 'app-delete-player',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeletePlayerComponent implements OnInit {
 
-  constructor() { }
+  constructor(private as: AdminService, private ar: ActivatedRoute, private route: Router) { }
 
+  id:number
+  playerdata: Player= {id:0, name:'',age:0, category:'',biddingPrice:0,teamId:0}
   ngOnInit(): void {
+    const tid= this.ar.snapshot.paramMap.get('id')
+    this.id= Number(tid)
+    this.Details(this.id)
   }
+  Details(id: number){
+    this.as.Details(id).subscribe((data : Player)=>
+    this.playerdata= data)}
+
+    saveData(player: Player): void{
+      this.playerdata = player
+      this.as.deletePlayer(this.id).subscribe(()=>
+      {
+        alert("Record Deleted")
+      })
+    }
 
 }
