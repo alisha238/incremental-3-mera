7ddf8e73aad8a8db495b95b3258646d54ac71062
Iamneo <techadmin@iamneo.ai>
2023-11-22 16:23:40 +0000

    Backup Commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { EditPlayerComponent } from './edit-player/edit-player.component';
 import { DeletePlayerComponent } from './delete-player/delete-player.component';
 import { DisplayTeamComponent } from './display-team/display-team.component';
 import { HomeComponent } from './home/home.component';
+import { TeamlistComponent } from './teamlist/teamlist.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { HomeComponent } from './home/home.component';
     EditPlayerComponent,
     DeletePlayerComponent,
     DisplayTeamComponent,
-    HomeComponent
+    HomeComponent,
+    TeamlistComponent
   ],
   imports: [
     BrowserModule,
--- angularapp/src/app/services/admin.service.ts
+++ angularapp/src/app/services/admin.service.ts
@@ -22,7 +22,7 @@ export class AdminService {
     return this.httpclient.get<Player>(this.url +'/GetPlayer/' + id);
   }
 
-  httpOptions= {headers:new HttpHeaders({'Content-type':'application/json'})}
+  // httpOptions= {headers:new HttpHeaders({'Content-type':'application/json'})}
   AddPlayer(playerdata:Player):Observable<Player>{
     return this.httpclient.post<Player>(this.url+'/AddPlayer',playerdata, this.httpOptions);
   }
@@ -35,11 +35,31 @@ export class AdminService {
     return this.httpclient.delete<Player>(this.url+'/DeletePlayer/' + id)
   }
 
-  GetAllTeams():Observable<any[]>
+  
+  getTeam():Observable<Team[]>
   {
-    return this.httpclient.get<any[]>(this.url + '/GetTeams');
+    return this.httpclient.get<Team[]>(this.url+ '/GetTeams')
+  }
+ 
+  findTeam(id:number):Observable<Team>
+  {
+    return this.httpclient.get<Team>(this.url+'/DisplayTeams/'+id)
   }
 
+  httpOptions= {headers:new HttpHeaders({'Content-type':'application/json'})}
+  addTeam(teamdata:Team):Observable<Team>
+  {
+    return this.httpclient.post<Team>(this.url+'/AddTeam',teamdata,this.httpOptions)
+  }
+ 
+  editTeam(teamdata:Team):Observable<Team>
+  {
+    return this.httpclient.put<Team>(this.url+'/EditTeam/'+teamdata.teamId,teamdata,this.httpOptions)
+  }
+ 
+  deleteTeam(id:number):Observable<Team>{
+    return this.httpclient.delete<Team>(this.url+'/DeleteTeam/'+id)
+  }
 
   
 }
